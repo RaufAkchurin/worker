bbd785ce2dfc9adada9635165c701cd734183a4a
rauf.akchurin <abdurauf.dev@gmail.com>
2023-11-09 21:40:24 +0500

    models v1

--- worker_app/admin.py
+++ worker_app/admin.py
@@ -1,3 +1,11 @@
+
+
 from django.contrib import admin
+from django.contrib.admin import site
+
+from worker_app.models import User, Object, WorkCategory
 
 # Register your models here.
+site.register(User)
+site.register(Object)
+site.register(WorkCategory)
--- worker_app/models.py
+++ worker_app/models.py
@@ -1,28 +1,49 @@
 from django.db import models
 
-from django.db import models
 
-
-class User(models.Model):
+class Worker(models.Model):
     name = models.CharField(max_length=10)
     surname = models.CharField(max_length=10)
     password = models.IntegerField()
+
+    def __str__(self):
+        return self.name
 
 
 class WorkCategory(models.Model):
     name = models.CharField(max_length=10)
 
+    def __str__(self):
+        return self.name
+
 
 class WorkType(models.Model):
     name = models.CharField(max_length=10)
-    category = models.ForeignKey(model=WorkCategory)
-    measurement = models.CharField(max_length=3)
+    category = models.ForeignKey(model=WorkCategory, on_delete=models.CASCADE)
+    value_type = models.CharField(max_length=3)
+
+    def __str__(self):
+        return self.name
 
 
 class Object(models.Model):
     name = models.CharField(max_length=10)
-    workers = models.ForeignKey(model=User)
+    workers = models.ForeignKey(model=Worker, on_delete=models.CASCADE)
     price_for_worker = models.IntegerField()
     price_for_customer = models.IntegerField()
     work_type = models.ForeignKey(model=WorkType)
     work_scope = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+
+class Work_shift(models.Model):
+    worker = models.ForeignKey(model=Worker, on_delete=models.PROTECT)
+    date = models.DateField()
+    work_type = models.OneToOneField(model=WorkType, on_delete=models.PROTECT)
+    value = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.worker.name} -- {self.date}"
+
