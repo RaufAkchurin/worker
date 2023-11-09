71c5c3b3389b774ad20538aff9f21a90a8c202b6
rauf.akchurin <abdurauf.dev@gmail.com>
2023-11-09 22:47:48 +0500

    models v2

--- worker_app/admin.py
+++ worker_app/admin.py
@@ -1,11 +1,11 @@
-
-
-from django.contrib import admin
 from django.contrib.admin import site
 
-from worker_app.models import User, Object, WorkCategory
+from worker_app.models import Worker, Category, CategorySub, WorkType, Object, Shift
 
 # Register your models here.
-site.register(User)
+site.register(Worker)
+site.register(Category)
+site.register(CategorySub)
+site.register(WorkType)
 site.register(Object)
-site.register(WorkCategory)
+site.register(Shift)
--- worker_app/models.py
+++ worker_app/models.py
@@ -7,41 +7,50 @@ class Worker(models.Model):
     password = models.IntegerField()
 
     def __str__(self):
+        return f"{self.surname} {self.name}"
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
         return self.name
 
 
-class WorkCategory(models.Model):
-    name = models.CharField(max_length=10)
+class CategorySub(models.Model):
+    category = models.OneToOneField(Category, on_delete=models.PROTECT)
+    name = models.CharField(max_length=255)
 
     def __str__(self):
         return self.name
 
 
 class WorkType(models.Model):
-    name = models.CharField(max_length=10)
-    category = models.ForeignKey(model=WorkCategory, on_delete=models.CASCADE)
-    value_type = models.CharField(max_length=3)
+    sub_category = models.OneToOneField(CategorySub, on_delete=models.PROTECT)
+    name = models.CharField(max_length=255)
+    value = models.IntegerField()
 
     def __str__(self):
         return self.name
 
 
 class Object(models.Model):
-    name = models.CharField(max_length=10)
-    workers = models.ForeignKey(model=Worker, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    workers = models.ForeignKey(Worker, on_delete=models.CASCADE)
     price_for_worker = models.IntegerField()
     price_for_customer = models.IntegerField()
-    work_type = models.ForeignKey(model=WorkType)
+    work_type = models.ForeignKey(WorkType, on_delete=models.PROTECT)
     work_scope = models.IntegerField()
 
     def __str__(self):
         return self.name
 
 
-class Work_shift(models.Model):
-    worker = models.ForeignKey(model=Worker, on_delete=models.PROTECT)
+class Shift(models.Model):
+    object = models.OneToOneField(Object, on_delete=models.PROTECT)
+    worker = models.ForeignKey(Worker, on_delete=models.PROTECT)
     date = models.DateField()
-    work_type = models.OneToOneField(model=WorkType, on_delete=models.PROTECT)
+    work_type = models.OneToOneField(WorkType, on_delete=models.PROTECT)
     value = models.IntegerField()
 
     def __str__(self):
