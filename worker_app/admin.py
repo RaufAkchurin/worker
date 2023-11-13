f020ce447d1574a48debb2ced65ee17707876e11
rauf.akchurin <abdurauf.dev@gmail.com>
2023-11-13 21:06:01 +0500

    django + PSTGRESS container v2

---
+++ worker_app/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.contrib.admin import site
+
+from worker_app.models import Worker, Category, CategorySub, WorkType, Object, Shift
+
+# Register your models here.
+
+site.register(Worker)
+site.register(Category)
+site.register(CategorySub)
+site.register(WorkType)
+site.register(Object)
+site.register(Shift)
