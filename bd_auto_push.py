0919965bb18ce121594c30b9f0533daa2f8e9e55
rauf.akchurin <abdurauf.dev@gmail.com>
2023-12-02 12:01:45 +0500

    bd_auto_push.py ver 6

--- bd_auto_push.py
+++ bd_auto_push.py
@@ -15,11 +15,28 @@ def push_to_github(repo_path, github_token, commit_message):
         file_path = "db.sqlite3"
         full_path = os.path.join(repo_path, file_path)
 
+        # Получение хэша последнего коммита в ветке
+        sha_latest_commit = repo.get_branch(branch).commit.sha
+
         with open(full_path, 'rb') as file_content:
             content = file_content.read()
 
-        # Создание файла без предоставления sha
-        repo.create_file(file_path, commit_message, content, branch=branch)
+        # Получение текущего содержимого файла
+        file_content_obj = repo.get_contents(file_path, ref=branch)
+        sha_current_content = file_content_obj.sha
+
+        # Создание нового коммита с обновленным файлом
+        repo.create_commit(
+            message=commit_message,
+            tree=repo.get_git_tree(sha_latest_commit),
+            parents=[sha_latest_commit],
+            updates=[{
+                "path": file_path,
+                "mode": "100644",
+                "content": content,
+                "sha": sha_current_content
+            }]
+        )
 
         print(f'Successfully pushed to GitHub at {time.ctime()}')
     except Exception as e:
