21f46c131b271b70f58fead77d7e794bbb0fb458
RaufAkchurin <abdurauf.dev@gmail.com>
2024-02-18 09:23:02 +0500

    test_db added

--- worker/settings.py
+++ worker/settings.py
@@ -67,12 +67,22 @@ WSGI_APPLICATION = 'worker.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+LOCALHOST_IP = os.getenv('LOCALHOST_IP')
+
+if LOCALHOST_IP in ['127.0.0.1:8000', '0.0.0.0:8000']:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db_test.sqlite3',
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
